e0246969a3416284867606f0fc24598781671914
dynby <dyominyuri@gmail.com>
2023-02-10 21:20:50 +0300

    brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+let b = str;
+for (let i = 0; i < str.length; i++){
+b = b.replaceAll("[]","").replaceAll("{}","").replaceAll("()","").replaceAll("||","").replaceAll("12","").replaceAll("34","").replaceAll("56","").replaceAll("77","").replaceAll("88","");
+}
+let lB = b.length;
+return lB === 0 ? true: false;
+
 }
